9efa971d2cb7c5b2fb9f19989515b9df95d7280b
Jason Vasquez <jasonwvasquez@gmail.com>
2026-05-10 18:47:40 -0400

    more mobile fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  // 1. The 'site' is your main domain
   site: 'https://jasonvasquez.github.io',
-  
-  // 2. The 'base' is your repository name (this is the magic fix)
   base: '/tiedwithkara',
-  
+  trailingSlash: 'always', // Add this line
   vite: {
     plugins: [tailwindcss()]
   }
